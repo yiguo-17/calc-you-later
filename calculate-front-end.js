5d887fca1f3980ccfe9399727a7b8fc1a171859e
play.js <git@gituser.com>
2020-06-23 14:30:51 -0400

    edit basic commands for front-end

--- calculate-front-end.js
+++ calculate-front-end.js
@@ -1,7 +1,11 @@
 /*********************************************
  * YOUR CODE HERE!                           *
  ********************************************/
-
+const calculate = require('./calculate-back-end.js');
+const num1 = getInput(0);
+const num2 = getInput(2);
+const operation = getInput(1);
+console.log(calculate(num1, num2, operation));
 
 
 
